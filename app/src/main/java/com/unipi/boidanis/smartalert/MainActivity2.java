c0ac22aa0185b3902d7d85aca335facfb50d2f1d
michalis-vslk <mixalisvslk22@gmail.com>
2023-02-07 22:27:42 +0200

    toast when alert submitted

--- app/src/main/java/com/unipi/boidanis/smartalert/MainActivity2.java
+++ app/src/main/java/com/unipi/boidanis/smartalert/MainActivity2.java
@@ -24,6 +24,7 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -124,6 +125,9 @@ public class MainActivity2 extends AppCompatActivity implements AdapterView.OnIt
             DangerData dangerData = new DangerData(key,dangerType, data.getText().toString(),gps.getLongitude(), gps.getLatitude(), currentTime,null,false,1);
 
             ref2.child(key).setValue(dangerData);
+            //showMessage("Success","Your danger alert has been submitted");
+            Toast.makeText(MainActivity2.this, "Your danger alert has been submitted!", Toast.LENGTH_SHORT).show();
+
         }else{
             showMessage("Error", "missing stuff");
         }
